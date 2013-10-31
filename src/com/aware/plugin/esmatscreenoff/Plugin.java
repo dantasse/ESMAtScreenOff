1ab029a0e62cb7fea622f44076ef2316fade98ee
Dan Tasse <dan.tasse@gmail.com>
2013-10-31 19:23:24 -0400

    first commit

---
+++ src/com/aware/plugin/esmatscreenoff/Plugin.java
@@ -0,0 +1,17 @@
+package com.aware.plugin.esmatscreenoff;
+
+import com.aware.utils.Aware_Sensor;
+
+public class Plugin extends Aware_Sensor {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Code here when add-on is turned on.
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Code here when add-on is turned off.
+    }
+}
