c519cda7f7ac8e5694e96c3a97a78cd8459323fb
Dan Tasse <dan.tasse@gmail.com>
2013-11-06 20:18:15 -0500

    now it can wake back up and buzz when it the screen turns off

--- src/com/aware/plugin/esmatscreenoff/Plugin.java
+++ src/com/aware/plugin/esmatscreenoff/Plugin.java
@@ -1,16 +1,32 @@
 package com.aware.plugin.esmatscreenoff;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
 import com.aware.utils.Aware_Sensor;
 
 public class Plugin extends Aware_Sensor {
+    
+    private BroadcastReceiver mReceiver = new ScreenReceiver();
+
+    
     @Override
     public void onCreate() {
+        Log.d("EsmAtScreenOff", "starting up!");
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mReceiver, filter);
+
         super.onCreate();
-        // Code here when add-on is turned on.
     }
 
     @Override
     public void onDestroy() {
+        Log.d("EsmAtScreenOff", "destroyin'!");
+        unregisterReceiver(mReceiver);
         super.onDestroy();
         // Code here when add-on is turned off.
     }
---
+++ src/com/aware/plugin/esmatscreenoff/ScreenReceiver.java
@@ -0,0 +1,40 @@
+package com.aware.plugin.esmatscreenoff;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.os.Vibrator;
+import android.util.Log;
+
+import com.aware.ESM;
+
+public class ScreenReceiver extends BroadcastReceiver {
+    
+    // see: http://thinkandroid.wordpress.com/2010/01/24/handling-screen-off-and-screen-on-intents/
+    public static boolean wasScreenOn = true;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+            Log.d("EsmAtScreenOff", "turning screen off");
+
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "EsmAtScreenOff");
+            wl.acquire(1000);
+            Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(500);
+
+            wasScreenOn = false;
+        } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+            Log.d("EsmAtScreenOff", "turning screen on");
+            wasScreenOn = true;
+        }
+        
+        Intent i = new Intent();
+        i.setAction(ESM.ACTION_AWARE_QUEUE_ESM);
+        String esmStr = "[{'esm': { 'esm_type': 1, 'esm_title': 'ESM Freetext', 'esm_instructions': 'The user can answer an open ended question.', 'esm_submit': 'Next', 'esm_expiration_threashold': 60, 'esm_trigger': 'AWARE Tester' }}]";
+        i.putExtra(ESM.EXTRA_ESM, esmStr);
+        context.sendBroadcast(i);
+    }
+}
