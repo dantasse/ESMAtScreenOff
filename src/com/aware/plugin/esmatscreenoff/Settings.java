027fd61795866a0cfbcb3bbdc49908d32ac14bc0
Dan Tasse <dan.tasse@gmail.com>
2013-12-11 12:24:06 -0500

    now there's a settings. doesn't do anything.

---
+++ src/com/aware/plugin/esmatscreenoff/Settings.java
@@ -0,0 +1,20 @@
+package com.aware.plugin.esmatscreenoff;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+}
